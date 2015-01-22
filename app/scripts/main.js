3a575f713b8d2f092c035898c860683167f41647
Spencer Wyckoff <spencer.wyckoff@gmail.com>
2015-01-22 18:25:22 -0500

    header modified, data pulled in

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('The Iron Yard Rocks');
+//DECLARE VARIABLES
+var image;
+var title;
+var shopName;
+var price;
+var currency;
+var listing;
+
+//DECLARE JQUERY SELECTOR FOR WHERE ITEMS WILL END UP ON THE DOM
+var listingsContainer = $('.container');
+
+//FOR EACH ITEM IN THE ARRAY, RUN THIS FUNCTION
+etsy.results.forEach( function (item) {
+
+//SET THE VARIABLES TO THEIR CORRESPONDING ITEMS
+image = item.Images[0].url_170x135;
+title = item.title;
+shopName = item.Shop.shop_name;
+price = item.price;
+currency = item.currency_code;
+
+console.log(image);
+
+//SET THE CONTENT OF THE ITEM BY CONCATENATING THEM TOGETHER
+listing = "<div class='listing'>"; 
+listing += "<img src='" + image + "'/>";
+listing += "<h4 class='l_title'>" + title + "</h4>";
+listing += "<span class='l_shop_name'>" + shopName + "</span>";
+
+
+listing += "<span class='l_price'>" + "$" + price + " " + currency + "</span>";
+listing += "</div>";
+
+//Select the HTML listings container and append listing to it over and over again, 25 times.
+listingsContainer.append(listing);
+
+//CLOSE THE FUNCTION
+});
+
+
+
+
+
+
+
